22a3e8adc570cfcc4794c9234a14aceeb863ded3
ozge.cetiner <ozge.cetiner@etiya.com>
2023-04-05 16:15:20 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,60 @@
+print("Merhaba Etiya")
+print("Hoşgeldiniz")
+
+# yorum satırı
+
+# değişkenler başlangıcı
+# metinsel, numerik, obje
+
+# string
+
+text= "Merhaba"
+
+print(text)
+print(text)
+print(text)
+text= "Selam"
+print(text)
+print(text)
+
+studentcount= "45" #string
+studentCount= 45 #int, integer => tamsayı
+print(studentCount + 5)
+
+averagePoint= 25.5 # double, decimal, float => ondalıklı sayı
+print(averagePoint + 5)
+
+isVerified= True # bool, boolean => True veya False
+print(isVerified)
+
+print(type(text))
+print(type(studentCount))
+print(type(averagePoint))
+print(type(isVerified))
+
+# değişkenler sonu
+
+# operatörler -start
+number= 10
+# matematiksel operatörler - mantıksal operatörler
+print(10 + 10)
+print(number + number)
+print(number + 10)
+print(number - 5)
+print(number / 2)
+print(number * 5)
+
+#mod operatörü => son taraftaki değerin sağ taraftaki değere bölümünden kalan sonuç
+print(number % 3)
+
+print(number + ((10 - number) * (5 / 10)))
+
+# mantıksal operatörler - karşılaştırmalı operatörler
+print(10 == 10) #true
+print(11 == 10) #false
+
+print(10 != 10) #false
+print(11 != 10) #true
+
+
+# operatörler -end
